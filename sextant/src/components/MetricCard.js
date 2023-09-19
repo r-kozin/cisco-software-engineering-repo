4337be3c980a08837a2cb2b96beb6b7c15041e4d
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-09-19 11:08:20 -0700

    added banner component and exhibit component that will display child components, completed task 2

---
+++ sextant/src/components/MetricCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MetricCard(props) {
+  return (
+    <div className="info-card">
+      <h2>Heading</h2>
+      <div className="info-child">
+        Child components
+      </div>
+    </div>
+  );
+}
